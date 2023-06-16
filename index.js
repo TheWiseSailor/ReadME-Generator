d35501c428b0bad1f2f68e706d301bd7d318e6ca
Joseph R <68026214+TheWiseSailor@users.noreply.github.com>
2023-06-16 20:35:46 +0000

    git it put in the wrong file

---
+++ index.js
@@ -0,0 +1,41 @@
+const questions = [
+  {
+      type:'input',
+      name:'email',
+      message: 'what is your email address?',
+  },
+  {
+      type: 'input',
+      name:'github',
+      message: 'What is your Github username?'
+  },
+
+{
+  type:'input',
+  name:'title',
+  message:'What is the title of your project?',
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'please provide a brief description of your project:',
+},
+
+{
+  type: 'input',
+  name: 'installation',
+  message: 'Please provide installation instructions for your project:',
+},
+{
+  type:'input',
+  name: 'usage',
+  message:'Please provide usage instructions for your project:',
+},
+{
+  type: 'list',
+  name: 'license',
+  choices: ['MIT', 'Apache','GPL', 'BSD'],
+
+},
+
+]
