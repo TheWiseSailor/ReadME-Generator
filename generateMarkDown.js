14d180cff24d8634d977acc3c126cd57e9fa251c
TheWiseSailor <rudasilljay@gmail.com>
2023-06-19 17:14:37 -0400

    finish tyhe stages of the generatemarkdown.... i think

--- generateMarkDown.js
+++ generateMarkDown.js
@@ -43,7 +43,10 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+${renderLicenseSection(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -41,5 +41,5 @@ const questions = [
 
 },
 
-]
 
+];
